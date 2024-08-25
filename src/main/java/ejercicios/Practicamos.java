d6a832162fb67ee60b32edb03837d5c87939b957
java-udm <huisaj30@gmail.com>
2024-08-25 12:41:18 -0500

    Rbucle for para calcular promedio de notas pseudocódigo

--- src/main/java/ejercicios/Practicamos.java
+++ src/main/java/ejercicios/Practicamos.java
@@ -10,7 +10,7 @@ public class Practicamos {
 
         // Sumar todos los elementos del array
         for (int i = 0; i < listaNotas.length; i++) {
-            promedio = promedio + listaNotas[i];
+            promedio = promedio + listaNotas[i]; // promedio = 0 + 12, promedio = 12 + 10, promedio = 22 + 10, promedio = 32 + 9
         }
 
         // Calcular el promedio
@@ -19,4 +19,22 @@ public class Practicamos {
         // Imprimir el promedio
         System.out.println(promedio);
     }
+//    Inicio
+//    // Declarar el array con las notas
+//    Definir listaNotas como un arreglo de enteros = {12, 10, 10, 9}
+//
+//    // Declarar la variable para el promedio
+//    Definir promedio como un número decimal = 0
+//
+//    // Sumar todos los elementos del array
+//    Para i desde 0 hasta el tamaño de listaNotas - 1 hacer
+//        promedio = promedio + listaNotas[i]
+//    Fin Para
+//
+//    // Calcular el promedio
+//    promedio = promedio / tamaño de listaNotas
+//
+//    // Imprimir el promedio
+//    Imprimir promedio
+//Fin
 }
